8741a06806782d01139bc87dfd01a8d32817f4df
sharee-tech <mail.sharee@gmail.com>
2023-10-19 20:55:19 -0500

    completed task two but still getting three errors on unit tests and post mappings are not working correctly

--- queries.sql
+++ queries.sql
@@ -8,6 +8,10 @@
 
 --Part 2
 
+--SELECT name
+--FROM employer
+--WHERE location = "St. Louis City";
+
 --Part 3
 
 --Part 4
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,22 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
+    @NotBlank(message = "Location is required")
+    @Size(min = 3, max = 50, message = "Location must be between 3 and 50 characters")
+    private String location;
+
+    public Employer() {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
